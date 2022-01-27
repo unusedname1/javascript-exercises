bb1e5ca1012660c71d97f207949f7de7132baf46
futurebcdev <zordevoanon@protonmail.com>
2022-01-27 17:32:16 -0500

    finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if(num < 0) return 'ERROR';
+    
+    let output = '';
+    while(num--) output += string;
+    return output;
 };
 
 // Do not edit below this line
