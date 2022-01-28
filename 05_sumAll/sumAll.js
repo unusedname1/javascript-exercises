66dbd1746be61f3163c29707b805ec6dac641e8c
futurebcdev <zordevoanon@protonmail.com>
2022-01-27 21:14:57 -0500

    finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(a < 0 || b < 0)
+        return 'ERROR';
+        
+    if(!Number.isInteger(a) || 
+       !Number.isInteger(b))
+        return 'ERROR';
 
+    const numTerms = Math.abs(b-a) + 1;
+    return numTerms * (a+b)/2;
 };
 
 // Do not edit below this line
