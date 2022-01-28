0f8b4861467f5805eaa2b829a40954a7873bea95
futurebcdev <zordevoanon@protonmail.com>
2022-01-27 20:25:50 -0500

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elements) {
+    for(const element of elements) {
+        const index = array.indexOf(element);
+        if(index !== -1 && element === array[index])
+            array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
